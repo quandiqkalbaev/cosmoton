6a16ebfd437dd5c26417d55977df5e0e2c4d015c
quandiqdev <quandiqdev@gmail.com>
2023-09-09 19:26:14 +0500

    ho

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,14 +4,24 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/images/logo.svg";
 import { ReactComponent as LangIcon } from "../../assets/images/lang.svg";
 import { useState } from "react";
+
 export default function Header() {
   const location = useLocation();
   const currentPath = location.pathname;
 
   const [menuOpen, setMenuOpen] = useState(false);
+  const [langmenuOpen, setLangMenuOpen] = useState(false);
 
   const handleMenuToggle = () => {
     setMenuOpen((prevState) => !prevState);
+  };
+
+  const handleLangMenuToggle = () => {
+    setLangMenuOpen((prevState) => !prevState);
+  };
+
+  const handleCloseLangMenu = () => {
+    setLangMenuOpen(false);
   };
 
   if (menuOpen) {
@@ -20,10 +30,6 @@ export default function Header() {
     document.body.classList.remove("no-scroll");
   }
 
-
-
-
-  
   return (
     <header className="header">
       <div className="container">
@@ -36,7 +42,9 @@ export default function Header() {
             <ul className="nav__list">
               <li
                 className={
-                  currentPath == "/game" || currentPath == "/catalog" ? `nav__item nav__item-active` : `nav__item`
+                  currentPath === "/game" || currentPath === "/catalog"
+                    ? `nav__item nav__item-active`
+                    : `nav__item`
                 }
               >
                 <Link to="/catalog" className="nav__link">
@@ -56,15 +64,19 @@ export default function Header() {
             </ul>
           </nav>
           <div className="header__lang">
-            <div className="header__lang-selected">
+            <div className="header__lang-selected" onClick={handleLangMenuToggle}>
               <span>
                 <LangIcon />
               </span>
               <p>Eng</p>
             </div>
-            <div className="header__lang-items">
-              <p className="header__lang-item">Русский</p>
-              <p className="header__lang-item">English</p>
+            <div className={langmenuOpen ? `header__lang-items header__lang-items-active` : `header__lang-items`}>
+              <p className="header__lang-item" onClick={handleCloseLangMenu}>
+                Русский
+              </p>
+              <p className="header__lang-item" onClick={handleCloseLangMenu}>
+                English
+              </p>
             </div>
           </div>
           <div className={menuOpen ? `hamburger hamburger--active` : `hamburger`} onClick={handleMenuToggle}>
